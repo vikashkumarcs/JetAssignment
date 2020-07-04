6960366cad9c21aff6e08c9e7087beb8b3a936dd
Vikash Kumar <vikash.kumar2-external@tcs.com>
2020-07-04 17:59:07 +0530

    > Added tableview > Design Custom cell UI

---
+++ JetAssignment/Utilities/AppConstant.swift
@@ -0,0 +1,11 @@
+//
+//  AppConstant.swift
+//  JetAssignment
+//
+//  Created by Vikash on 04/07/20.
+//  Copyright © 2020 Vikash. All rights reserved.
+//
+
+import Foundation
+
+// https://5e99a9b1bc561b0016af3540.mockapi.io/jet2/api/v1/blogs?page= 1&limit=10
